d27509e48328b6216530904de1b7d2f3ea624501
Dudecake <coen_koomen@hotmail.com>
2024-05-23 20:54:44 +0000

    Added wrapper subcommand

--- zig-wrapper.sh
+++ zig-wrapper.sh
@@ -24,13 +24,33 @@ set -e
 
 . .zig/wrapper/zig-wrapper.properties
 wrapper_directory="\${HOME}/.cache/zig/wrapper/\${zig_version}"
-if [[ ! -f \${wrapper_directory}/zig || "\$(sh -c "\${wrapper_directory}/zig version" 2> /dev/null)" != "\${zig_version}" ]]; then
-  [[ -d \${wrapper_directory} ]] || mkdir -p \${wrapper_directory}
-  arch="\$(uname -m)"
-  curl -L https://ziglang.org/download/\${zig_version}/zig-linux-\${arch}-\${zig_version}.tar.xz | bsdtar -C \${wrapper_directory} --strip-components 1 -xf -
-  curl -L https://github.com/zigtools/zls/releases/download/\${zig_version}/zls-\${arch}-linux.tar.xz | bsdtar -C \${wrapper_directory} -xf - zls
-fi
+if [[ "\${1}" = "wrapper" ]]; then
+  case "\${2}" in
+    version)
+      cat << EOV >&2
+Zig version:      \${zig_version}
+Zig home:         \${wrapper_directory}
+Wrapper version:  0.0.3
+EOV
+      ;;
+    *)
+      cat << EOH >&2
+info: Usage: \${0} wrapper [command]
 
-exec \${wrapper_directory}/zig "\$@"
+Commands:
+  version     Print version number and exit
+EOH
+      ;;
+  esac
+else
+  if [[ ! -f \${wrapper_directory}/zig || "\$(sh -c "\${wrapper_directory}/zig version" 2> /dev/null)" != "\${zig_version}" ]]; then
+    [[ -d \${wrapper_directory} ]] || mkdir -p \${wrapper_directory}
+    arch="\$(uname -m)"
+    curl -L https://ziglang.org/download/\${zig_version}/zig-linux-\${arch}-\${zig_version}.tar.xz | bsdtar -C \${wrapper_directory} --strip-components 1 -xf -
+    curl -L https://github.com/zigtools/zls/releases/download/\${zig_version}/zls-\${arch}-linux.tar.xz | bsdtar -C \${wrapper_directory} -xf - zls
+  fi
+
+  exec \${wrapper_directory}/zig "\$@"
+fi
 EOF
 chmod +x ./zigw
