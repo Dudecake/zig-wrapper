30bedbd5cdce5db81e6f17f3b98b49b396495e63
Dudecake <ckoomen@ckoomen.eu>
2024-10-08 19:16:37 +0000

    Split `zlsw` from `zigw`

--- zig-wrapper.sh
+++ zig-wrapper.sh
@@ -13,7 +13,7 @@ fi
 
 if [[ -f zigw ]]; then
   echo "Wrapper already installed" >&2
-  exit 0
+  [[ "$(basename $PWD)" = "zig-wrapper" ]] || exit 0
 fi
 
 [[ -d .zig/wrapper ]] || mkdir -p .zig/wrapper
@@ -21,6 +21,7 @@ cat << EOF > .zig/wrapper/zig-wrapper.properties
 zig_version=0.13.0
 EOF
 
+wrapper_version="0.0.5"
 cat << EOF > zigw
 #!/bin/sh
 
@@ -36,9 +37,11 @@ if [[ "\${1}" = "wrapper" ]]; then
       cat << EOV >&2
 Zig version:      \${zig_version}
 Zig home:         \${wrapper_directory}
-Wrapper version:  0.0.4
+Wrapper version:  ${wrapper_version}
 EOV
       ;;
+    # update)
+    #   ;;
     *)
       cat << EOH >&2
 info: Usage: \${0} wrapper [command]
@@ -53,10 +56,49 @@ else
     [[ -d \${wrapper_directory} ]] || mkdir -p \${wrapper_directory}
     arch="\$(uname -m)"
     curl -L https://ziglang.org/download/\${zig_version}/zig-linux-\${arch}-\${zig_version}.tar.xz | bsdtar -C \${wrapper_directory} --strip-components 1 -xf -
-    curl -L https://github.com/zigtools/zls/releases/download/\${zig_version}/zls-\${arch}-linux.tar.xz | bsdtar -C \${wrapper_directory} -xf - zls
   fi
 
   exec \${wrapper_directory}/zig "\$@"
 fi
 EOF
 chmod +x ./zigw
+
+cat << EOF > zlsw
+#!/bin/sh
+
+set -e
+
+script_dir=\$( cd -- "\$( dirname -- "\${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+. ${script_dir}/.zig/wrapper/zig-wrapper.properties
+wrapper_directory="\${HOME}/.cache/zig/wrapper/\${zig_version}"
+if [[ "\${1}" = "wrapper" ]]; then
+  case "\${2}" in
+    version)
+      cat << EOV >&2
+Zig version:      \${zig_version}
+Zig home:         \${wrapper_directory}
+Wrapper version:  ${wrapper_version}
+EOV
+      ;;
+    # update)
+    #   ;;
+    *)
+      cat << EOH >&2
+info: Usage: \${0} wrapper [command]
+
+Commands:
+  version     Print version number and exit
+EOH
+      ;;
+  esac
+else
+  if [[ ! -f \${wrapper_directory}/zls || "\$(sh -c "\${wrapper_directory}/zls version" 2> /dev/null)" != "\${zig_version}" ]]; then
+    [[ -d \${wrapper_directory} ]] || mkdir -p \${wrapper_directory}
+    arch="\$(uname -m)"
+    curl -L https://github.com/zigtools/zls/releases/download/\${zig_version}/zls-\${arch}-linux.tar.xz | bsdtar -C \${wrapper_directory} -xf - zls
+  fi
+
+  exec \${wrapper_directory}/zls "\$@"
+EOF
+chmod +x ./zlsw
